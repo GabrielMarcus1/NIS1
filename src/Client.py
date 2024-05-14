f21ea8aacd0840eaaea884f9658985037376d823
Gabriel <gabemarcus18@gmail.com>
2024-05-14 14:16:39 +0200

    combined network and encryption

--- src/Client.py
+++ src/Client.py
@@ -1,84 +1,66 @@
 import socket
-import pickle
+import os
+import tkinter as tk
+from tkinter import simpledialog, filedialog, messagebox
+import base64
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import serialization, hashes
-from cryptography.hazmat.primitives.asymmetric import rsa, padding
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives import serialization, rsa
 
 class Client:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, master, host, port):
+        self.master = master
+        self.host = host
+        self.port = port
+        self.setup_ui()
         self.client_private_key = rsa.generate_private_key(
             public_exponent=65537,
             key_size=2048,
             backend=default_backend()
         )
         self.client_public_key = self.client_private_key.public_key()
-        self.server_public_key = None
-        self.shared_key = None
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connect()
 
-    def connect(self, host, port):
-        self.socket.connect((host, port))
+    def setup_ui(self):
+        self.master.title("Client Interface")
+        tk.Label(self.master, text="Select Action:").pack()
+        tk.Button(self.master, text="Send Text", command=self.send_text).pack()
+        tk.Button(self.master, text="Upload Image", command=self.send_image).pack()
+        tk.Button(self.master, text="Exit", command=self.close_connection).pack()
 
-    def send_public_key(self):
+    def connect(self):
+        self.socket.connect((self.host, self.port))
         self.socket.send(self.client_public_key.public_bytes(
             encoding=serialization.Encoding.PEM,
             format=serialization.PublicFormat.SubjectPublicKeyInfo
         ))
 
-    def receive_server_public_key(self):
-        server_public_key_bytes = self.socket.recv(4096)
-        self.server_public_key = serialization.load_pem_public_key(
-            server_public_key_bytes,
-            backend=default_backend()
-        )
+    def send_text(self):
+        text = simpledialog.askstring("Input", "Enter your message:", parent=self.master)
+        if text:
+            encoded_text = base64.b64encode(text.encode()).decode()
+            self.socket.sendall('TEXT'.encode())
+            self.socket.sendall(str(len(encoded_text)).encode())
+            self.socket.sendall(encoded_text.encode())
 
-    def receive_shared_key(self):
-        self.shared_key = self.socket.recv(4096)
-
-    def derive_aes_key(self):
-        hkdf = HKDF(
-            algorithm=hashes.SHA256(),
-            length=32,  # 256-bit key
-            salt=None,
-            info=b'',
-            backend=default_backend()
-        )
-        return hkdf.derive(self.shared_key)
-
-    def send_encrypted_message(self, message):
-        aes_key = self.derive_aes_key()
-        cipher = Cipher(algorithms.AES(aes_key), modes.CTR(b'\x00' * 16), backend=default_backend())
-        encryptor = cipher.encryptor()
-        encrypted_message = encryptor.update(message.encode()) + encryptor.finalize()
-        self.socket.send(encrypted_message)
-
-    def receive_encrypted_message(self):
-        aes_key = self.derive_aes_key()
-        encrypted_message = self.socket.recv(4096)
-        cipher = Cipher(algorithms.AES(aes_key), modes.CTR(b'\x00' * 16), backend=default_backend())
-        decryptor = cipher.decryptor()
-        decrypted_message = decryptor.update(encrypted_message) + decryptor.finalize()
-        return decrypted_message.decode()
+    def send_image(self):
+        filepath = filedialog.askopenfilename(title="Select image", filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png"), ("all files", "*.*")))
+        if filepath:
+            with open(filepath, 'rb') as file:
+                image_data = file.read()
+            base64_encoded_data = base64.b64encode(image_data).decode()
+            self.socket.sendall('IMAGE'.encode())
+            self.socket.sendall(str(len(base64_encoded_data)).encode())
+            self.socket.sendall(base64_encoded_data.encode())
 
     def close_connection(self):
         self.socket.close()
+        self.master.quit()
 
 def main():
-    alice = Client("Alice")
-    alice.connect("localhost", 12345)
-    alice.send_public_key()
-    alice.receive_server_public_key()
-    alice.receive_shared_key()
-    message = "Hello from Alice!"
-    print("Original Message:", message)
-    alice.send_encrypted_message(message)
-    received_message = alice.receive_encrypted_message()
-    print("Received Message:", received_message)
-    alice.close_connection()
+    root = tk.Tk()
+    client = Client(root, "localhost", 12345)
+    root.mainloop()
 
 if __name__ == "__main__":
     main()
