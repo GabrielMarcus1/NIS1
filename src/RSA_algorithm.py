b9293f949b360c14c64744a48b12da4ef998aa7f
Shivaskar Naidoo <113246197+Shivbot85@users.noreply.github.com>
2024-05-14 13:51:32 +0200

    Add files via upload
    GUI Components added

--- src/RSA_algorithm.py
+++ src/RSA_algorithm.py
@@ -1,5 +1,3 @@
-import base64
-import json
 import secrets
 import cryptography
 from cryptography.hazmat.primitives.asymmetric import rsa # type: ignore
@@ -8,11 +6,9 @@ from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 import hashlib
-from cryptography.hazmat.primitives.padding import PKCS7
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 import os
-import compress_message
 
 
 ############# KEY GENERATION,ENCRYPTION AND DECRYPTION (PRIVATE & PUBLIC KEYS)####################
@@ -36,13 +32,13 @@ def gen_public_key(private_key):
     return public_key
 
 # encrypte message using other users private key
-def rsa_encrypt(message, public_key):
+def encrypt(message, public_key):
     """
     :param message:
     :param public_key:
     :return: Cipher text
     """
-    # message = message.encode("utf-8")
+    message = message.encode("utf-8")
     cipher = public_key.encrypt( message,padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -51,7 +47,7 @@ def rsa_encrypt(message, public_key):
     )
     return cipher
 
-def rsa_decrypt(cipher, private_key):
+def decrypt(cipher, private_key):
     """
     This function takes cipher (to be decoded after decryption) and generated private key as input and returns the
     plaintext.
@@ -66,7 +62,7 @@ def rsa_decrypt(cipher, private_key):
         )
     )
 
-    return plaintext
+    return plaintext.decode("utf8")
 ###################################################################
 
 #################SECRET KEY Generation, Decryption Encryption#########################################
@@ -74,34 +70,31 @@ def generate_secret_key():
     # Generate a random 32-byte (256-bit) key
     return os.urandom(32)
 
-def aes_encrypt_message(message, secret_key):
+def encrypt_message(message, secret_key):
     """
-    Encrypts the message using AES-CBC mode.
+    This function takes a message and a secret key as input and encrypts the message using AES-CBC mode.
     Parameters:
-    message (bytes): The message to be encrypted.
+    message (str): The message to be encrypted.
     secret_key (bytes): The secret key used for encryption.
     Returns:
     bytes: The IV and ciphertext.
     """
     # Generate a random IV (Initialization Vector)
     iv = os.urandom(16)
-
+    
+    # make the message paramter into byte form
+    message_bytes = message.encode("utf-8")
     # Create an AES cipher with CBC mode using the secret key and IV
     cipher = Cipher(algorithms.AES(secret_key), modes.CBC(iv), backend=default_backend())
+    # Encrypt the message
     encryptor = cipher.encryptor()
-
-    # Apply PKCS7 padding to ensure message length is a multiple of the block size
-    padder = PKCS7(algorithms.AES.block_size).padder()
-    padded_message = padder.update(message) + padder.finalize()
-
-    # Encrypt the padded message
-    ciphertext = encryptor.update(padded_message) + encryptor.finalize()
-
+    #print(encryptor.update(message))
+    ciphertext = encryptor.update(message_bytes) + encryptor.finalize()
     # Return the IV and ciphertext
     return iv + ciphertext
 
 
-def aes_decrypt_message(encrypted_message, secret_key):
+def decrypt_message(encrypted_message, secret_key):
     """
     This function takes an encrypted message and a secret key as input and decrypts the message using AES-CBC mode.
     Parameters:
@@ -121,11 +114,20 @@ def aes_decrypt_message(encrypted_message, secret_key):
     # Decrypt the ciphertext
     decryptor = cipher.decryptor()
     decrypted_message = decryptor.update(ciphertext) + decryptor.finalize()
+    print(decrypted_message)
     return decrypted_message
 
 ######################################################################
+
+
+######################################################################
+
 ############KEY MANAGEMENT#######################
+# Save Key to file. Key is in PEM format
 def save_key(key, filename, key_type):
+    """
+    Save key to file
+    """
     with open("keys/"+filename, "wb") as f:
         if key_type == "private":
             f.write(key.private_bytes(
@@ -141,6 +143,7 @@ def save_key(key, filename, key_type):
         else:
             raise ValueError("Invalid key type. Must be 'private' or 'public'.")
 
+#Load key from PEM file 
 def load_key(filename, key_type):
     """
     Load key from PEM file
@@ -165,73 +168,33 @@ def load_key(filename, key_type):
 
 
 ############HASHING########################
-# def hash(message):
-#     """
-#     This function takes a message as input and returns the hexadecimal digest of the SHA-256 hash of the message.
-#     Parameters:
-#     message (str): The message to be hashed.
+def hash(message):
+    """
+    This function takes a message as input and returns the hexadecimal digest of the SHA-256 hash of the message.
+    Parameters:
+    message (str): The message to be hashed.
 
-#     Returns:
-#     str: The hexadecimal digest of the SHA-256 hash of the message.
-#     """
-#     sha256_hash = hashlib.sha256()
-#     sha256_hash.update(message)
-#     # Get the hexadecimal digest of the hash
-#     hex_digest = sha256_hash.hexdigest()
-#     # digest = sha256_hash.digest() 
-#     # ^^^^This is smaller than hexdigest, its half thwe size and should be used once we are done debugging
-#     return hex_digest
-
-# def verify_hash(message, hex_digest):
-#     """
-#     This function takes a message and a hexadecimal digest as input and returns True if the hexadecimal digest is the SHA-256 hash of the message, and False otherwise.
-#     Parameters:
-#     message (str): The message to be hashed.
-#     hex_digest (str): The hexadecimal digest to be verified.
-
-#     Returns:
-#     bool: True if the hexadecimal digest is the SHA-256 hash of the message, and False otherwise.
-#     """
-#     return hash(message) == hex_digest  # Compare the hash of the message with the given hexadecimal digest
-##################################
-#################DECRYPTION#########################
-
-
-def decrypt_message_PGP(message, private_key):
-    session_key = (message["session_key"])
-    
-    decrypted_session_key = rsa_decrypt(session_key, private_key)
-    encrypted_file = (message["encrypted_file"])
-   
-    decrypted_file= aes_decrypt_message(encrypted_file, decrypted_session_key)
-
-    # print(decrypted_file)
-    file= compress_message.decompress_signature_and_message(decrypted_file)
-   
-    signature= file['signature']
-    digest = signature['Digest']
-    
-    message=(file["signature"])
-    print(message)
-    
-    message = file["message"]
-
-    # print(message)
-    message = json.dumps(message)
-    message_json = json.dumps(message, sort_keys=True).encode('utf-8')
+    Returns:
+    str: The hexadecimal digest of the SHA-256 hash of the message.
+    """
     sha256_hash = hashlib.sha256()
-    sha256_hash.update(message_json)
-
+    sha256_hash.update(message)
     # Get the hexadecimal digest of the hash
     hex_digest = sha256_hash.hexdigest()
-    # message=message.decode()	
-   
-    print(hex_digest)
-    
+    # digest = sha256_hash.digest() 
+    # ^^^^This is smaller than hexdigest, its half thwe size and should be used once we are done debugging
+    return hex_digest
 
-   
+def verify_hash(message, hex_digest):
+    """
+    This function takes a message and a hexadecimal digest as input and returns True if the hexadecimal digest is the SHA-256 hash of the message, and False otherwise.
+    Parameters:
+    message (str): The message to be hashed.
+    hex_digest (str): The hexadecimal digest to be verified.
 
-       
-    # decrypted_file = aes_decrypt_message(encrypted_file, session_key)
-    # print(decrypted_file)
+    Returns:
+    bool: True if the hexadecimal digest is the SHA-256 hash of the message, and False otherwise.
+    """
+    return hash(message) == hex_digest  # Compare the hash of the message with the given hexadecimal digest
+##################################
 
